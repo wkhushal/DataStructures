701fbeb48c04b728bdbcc568d2be129349e6707c
Waqas Khushal <waqas.khushal@bp.com>
2020-05-02 22:48:46 +0100

    BinarySearchTree with Add, Search and basic UnitTests

---
+++ DataStructure/BinaryTrees/BinarySearchTree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.BinaryTrees
+{
+    public class BinarySearchTree
+    {
+        public BinarySearchTree(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; private set; }
+        public BinarySearchTree Left { get; private set; }
+        public BinarySearchTree Right { get; private set; }
+
+        public void AddValue(int value)
+        {
+            if(value == Value)
+            {
+                return;
+            }
+
+            if (value < Value)
+            {
+                AddLeftValue(value);
+            }
+            else
+            {
+                AddRightValue(value);
+            }
+        }
+
+        public BinarySearchTree Search(int value)
+        {
+            if (value == Value)
+            {
+                return this;
+            }
+            
+            if (value < Value)
+            {
+                return Left?.Search(value);
+            }
+
+            return Right?.Search(value);
+        }
+
+        private void AddLeftValue(int value)
+        {
+            if (Left is null)
+            {
+                Left = new BinarySearchTree(value);
+            }
+            else
+            {
+                Left.AddValue(value);
+            }
+        }
+
+        private void AddRightValue(int value)
+        {
+            if (Right is null)
+            {
+                Right = new BinarySearchTree(value);
+            }
+            else
+            {
+                Right.AddValue(value);
+            }
+        }
+    }
+}
---
+++ DataStructureTests/BinaryTrees/BinarySearchTreesTests.cs
@@ -0,0 +1,97 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using DataStructure.BinaryTrees;
+using System;
+using Xunit;
+
+
+namespace DataStructureTests.BinaryTrees
+{
+    public class BinarySearchTreesTests
+    {
+        [Fact]
+        public void BinarySearchTrees()
+        {
+            var value = 5;
+            var result = new BinarySearchTree(value);
+            Assert.NotNull(result);
+            Assert.Equal(value, result.Value);
+        }
+
+        [Theory, AutoData]
+        public void AddValue(int value)
+        {
+            var sut = new BinarySearchTree(value);
+
+            sut.AddValue(value);
+
+            Assert.Equal(value, sut.Value);
+            Assert.Null(sut.Left);
+            Assert.Null(sut.Right);
+        }
+
+        [Fact]
+        public void AddLeftValue()
+        {
+            var rootValue = 5;
+            var newValue = 3;
+            var sut = new BinarySearchTree(rootValue);
+
+            sut.AddValue(newValue);
+
+            Assert.NotNull(sut.Left);
+            Assert.Equal(newValue, sut.Left.Value);
+            Assert.Null(sut.Right);
+        }
+
+        [Fact]
+        public void AddRightValue()
+        {
+            var rootValue = 2;
+            var newValue = 3;
+            var sut = new BinarySearchTree(rootValue);
+
+            sut.AddValue(newValue);
+
+            Assert.NotNull(sut.Right);
+            Assert.Equal(newValue, sut.Right.Value);
+            Assert.Null(sut.Left);
+        }
+
+        [Theory, AutoData]
+        public void Search(int value)
+        {
+            var sut = new BinarySearchTree(value);
+
+            var result = sut.Search(value);
+
+            Assert.NotNull(result);
+            Assert.Same(sut, result);
+        }
+
+        [Fact]
+        public void SearchLeftNull()
+        {
+            var rootValue = 5;
+            var searchValue = 4;
+            var sut = new BinarySearchTree(rootValue);
+
+            var result = sut.Search(searchValue);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void SearchRightNull()
+        {
+            var rootValue = 5;
+            var searchValue = 6;
+            var sut = new BinarySearchTree(rootValue);
+
+            var result = sut.Search(searchValue);
+
+            Assert.Null(result);
+        }
+
+    }
+}
